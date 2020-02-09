0a8fe801e68e44b45600682a9a20ccff96c43a8c
Isaev Aleksey <isaev.alex75311@gmail.com>
2020-02-09 03:43:56 +0300

    Оповещение в телеграмм

---
+++ conf.py
@@ -0,0 +1,5 @@
+TOKEN = YOUR_TOKEN
+proxy = YOUR_PROXY
+chat_id = TOUR_CHAT_ID (int)
+split_chr = '\\'    # для винды '\\', для линукса '/'
+path = YOUR_PATH
--- file_tracking.py
+++ file_tracking.py
@@ -2,22 +2,34 @@ from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 import time
 import os
+import telebot
+from telebot import apihelper
+from conf import TOKEN, proxy, chat_id, split_chr, path
+
+apihelper.proxy = {'https': proxy}
+bot = telebot.TeleBot(TOKEN)
+
+
+def send_message(*args):
+    message = args[0]
+    bot.send_message(chat_id, message)
 
 
 class Handler(FileSystemEventHandler):
     def on_created(self, event):
-        print(event)
-        print(event.src_path.split('\\')[-1])
+        if os.path.isfile(event.src_path):
+            print(event.src_path.split(split_chr)[-1])
+            send_message(event.src_path.split('\\')[-1])
+
+
+@bot.message_handler(commands=['start'])
+def send_echo(message):
+    bot.send_message(message.chat.id, 'Работаю')
 
 
 if __name__ == '__main__':
-    path = os.getcwd()
     observer = Observer()
     observer.schedule(Handler(), path=path, recursive=True)
     observer.start()
-    try:
-        while True:
-            time.sleep(0.1)
-    except KeyboardInterrupt:
-        observer.stop()
-    observer.join()
+    bot.send_message(chat_id, 'Бот запущен')
+    bot.polling()
